693b11437a08e8e8915142ea58a922440969307f
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2023-12-05 21:57:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#contactForm').submit(function(e) {
+        e.preventDefault();
+        alert('Thank you for reaching out! We will get back to you soon.');
+    });
+});
